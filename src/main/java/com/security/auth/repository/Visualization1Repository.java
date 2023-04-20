10c21ff1bf765b091af97fe965e56da43fcb83c8
LassiVaisanen <t1vala02@students.oamk.fi>
2023-04-20 17:49:20 +0300

    Update Visualization 1

--- src/main/java/com/security/auth/repository/Visualization1Repository.java
+++ src/main/java/com/security/auth/repository/Visualization1Repository.java
@@ -3,19 +3,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.security.auth.data.Visualization1;
+import com.security.auth.data.Visualization1Annual;
 
 import java.util.List;
 import java.util.Map;
 
 @Repository
-public interface Visualization1Repository extends JpaRepository<Visualization1, Integer>{
+public interface Visualization1Repository extends JpaRepository<Visualization1Annual, Integer>{
     //custom query to search data by time
-    List<Visualization1> findByTime(String time);
+    List<Visualization1Annual> findByTime(String time);
     //List<Visualization1> findAll();
 
     @Query(value="SELECT * FROM annual", nativeQuery = true)
-    List<Map<String, Object>> getAll();
+    List<Map<String, Object>> getAnnual();
+
+    @Query(value="SELECT * FROM monthly", nativeQuery = true)
+    List<Map<String, Object>> getMonthly();
+
+    @Query(value="SELECT * FROM reconstruction", nativeQuery = true)
+    List<Map<String, Object>> getReconstruction();
     
     //void deleteById(String time);
 }
