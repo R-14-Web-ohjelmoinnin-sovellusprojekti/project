1ab59033b426cccf4c3f4cfd15a370d7d0e95fcf
Emilia Virta <virta.emilia@gmail.com>
2023-05-02 21:12:58 +0300

    update to about site

--- auth-client/src/components/About.js
+++ auth-client/src/components/About.js
@@ -4,10 +4,20 @@ import React from 'react'
 export default function Home() {
     return (
         <div className="project-description">
-            <h1>Project ClimateView</h1>
-            <p>Introduction</p>
-            <p>Tämä on Oulun ammattikorkeakoulun toisen vuoden ohjelmistokehityksen opiskelijoiden
-                tekemä ilmastonmuutoksen visualisointi-työkalun alku. </p>
+            <h2>Esittely</h2>
+            <p>
+            Tämä on Oulun ammattikorkeakoulun 2. vuoden ohjelmistokehityksen opiskelijoiden toteuttama web-ohjelmoinnin sovellusprojekti.
+            Tavoitteena projektissa oli suunnitella ja ohjelmoida ilmastonmuutoksen datavisualisointityökalu. 
+            </p>
+            <p>
+            Projekti tehtiin ryhmätyönä jaotellen jokaiselle ryhmän jäsenelle omat osa-alueet. 
+            Projektin kaikki jäsenet toimivat Full Stack -kehittäjinä työstäen sekä frontend- että backend-puolta.
+            </p>
+            <p>
+            Projektissa käytettiin Java-ohjelmointikieltä tietokantapalvelimen toteutukseen ja JavaScriptiä ja sen React-kirjastoa datan visualisointiin selaimessa.
+            Ohjelmointiympäristönä projektissa käytettiin Visual Studio Codea. Tietokantaohjelmistona käytettiin MySQL-ohjelmistoa. 
+            Java koodissa toteutettiin tietokantahaut, joita voidaan käyttää HTTP-kutsuilla JavaScript-koodissa
+            </p>
         </div>
     );
 }
